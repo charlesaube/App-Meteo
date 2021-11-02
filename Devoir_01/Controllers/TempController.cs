128a72f9d0281c5d3f67c6c17070dc83674ce91d
charl <charlesetienne.aube@gmail.com>
2021-11-02 14:45:35 -0400

    Ajoutez des fichiers projet.

---
+++ Devoir_01/Controllers/TempController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Devoir_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Devoir_01.Controllers
+{
+    public class TempController : Controller
+    {
+
+        List<Temperature> temperatures;
+        DateTime today;
+
+        public TempController()
+        {
+            this.today = DateTime.Now;
+            this.temperatures = Temperature.Init();
+        }
+
+        public IActionResult Index()
+        {
+            Temperature todayTemp = temperatures.Where(t => t.Date.Year == this.today.Year 
+            && t.Date.Month == this.today.Month && t.Date.Day == this.today.Day).Single();
+
+            return View(todayTemp);
+        }
+
+        public IActionResult Previsions(int n)
+        {
+            List<Temperature> selected = temperatures.Where(t => t.Date.DayOfYear > this.today.DayOfYear && t.Date.DayOfYear <= this.today.DayOfYear + n).ToList();
+            return View(selected);
+        }
+
+        public IActionResult Stats(int mois)
+        {
+            if (mois < 1)
+                mois = today.Date.Month;
+
+            List<Temperature> tempFromMonth = temperatures.Where(t => t.Date.Month == mois).ToList();
+            string maxTempFromMonth = tempFromMonth.Max(t => t.MaxTemp).ToString();
+            string minTempFromMonth = tempFromMonth.Min(t => t.MinTemp).ToString();
+            string averageTempFromMonth =Convert.ToInt32(tempFromMonth.Average(t => t.Temp)).ToString();
+            
+            ViewBag.maxTemp = maxTempFromMonth;
+            ViewBag.minTemp = minTempFromMonth;
+            ViewBag.averageTemp = averageTempFromMonth;
+            ViewBag.months = new List<SelectListItem> {
+                        new SelectListItem { Value = "1", Text = "Janvier" },
+                        new SelectListItem { Value = "2", Text = "Février" },
+                        new SelectListItem { Value = "3", Text = "Mars" },
+                        new SelectListItem { Value = "4", Text = "Avril" },
+                        new SelectListItem { Value = "5", Text = "Mai" },
+                        new SelectListItem { Value = "6", Text = "Juin" },
+                        new SelectListItem { Value = "7", Text = "Juillet" },
+                        new SelectListItem { Value = "8", Text = "Août" },
+                        new SelectListItem { Value = "9", Text = "Septembre" },
+                        new SelectListItem { Value = "10", Text = "Octobre" },
+                        new SelectListItem { Value = "11", Text = "Novembre" },
+                        new SelectListItem { Value = "12", Text = "Décembre" },
+            };
+            return View(tempFromMonth);
+        }
+    }
+}
---
+++ Devoir_01/Models/Temperature.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Devoir_01.Models
+{
+    public class Temperature
+    {
+        public DateTime Date { get; set; }
+        public int MaxTemp { get; set; }
+        public int MinTemp { get; set; }
+        public int Temp { get; set; }
+
+        public static List<Temperature> Init()
+        {
+            List<Temperature> temperatures = new List<Temperature>();
+
+            // Lecture du fichier et itération sur les lignes lues
+            // File.ReadAllLines crée un tableau de string
+            // représentant chaque ligne
+            int count = 0;
+            foreach (var line in File.ReadAllLines("weather_montreal.txt"))
+            {
+                // Certaines lignes sont vides
+                // et doivent être filtrées
+                if (string.IsNullOrEmpty(line) || count == 0)
+                {
+                    count++;
+                    continue;
+                }
+                // split construit un tableau de string à partir
+                // d'une ligne découpée suivant les tabulations
+                var entries = line.Split('\t');
+
+
+                // Ajout de l'instance dans le conteneur
+                temperatures.Add(new Temperature
+                {
+                    Date = new DateTime(int.Parse(entries[0]),int.Parse(entries[1]),int.Parse(entries[2])),
+                    MaxTemp = int.Parse(entries[3]),
+                    MinTemp = int.Parse(entries[4]),
+                    Temp = int.Parse(entries[5])
+                });
+            }
+            // La liste d'objets est lue, on peut commencer à jouer
+            return temperatures;
+        }
+    }
+
+
+}
